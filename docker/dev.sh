15116cd74e302c8868d0d53946c1c743d85d9b20
Ryan <31500999+taihtm@users.noreply.github.com>
2023-01-10 22:24:52 +0700

    Update docker compose script

--- docker/dev.sh
+++ docker/dev.sh
@@ -2,6 +2,6 @@
 
 _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-exec docker-compose -p buidler-documentation-dev \
+exec docker compose -p buidler-documentation-dev \
   -f "$_dockerDir/compose.dev.yml" \
   "$@"
--- docker/prod.sh
+++ docker/prod.sh
@@ -2,6 +2,6 @@
 
 _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-exec docker-compose -p buidler-documentation-prod \
+exec docker compose -p buidler-documentation-prod \
   -f "$_dockerDir/compose.prod.yml" \
   "$@"
