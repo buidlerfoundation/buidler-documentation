d489c16bdac575d22a3954d973e2b902ba86a0db
Ryan <31500999+taihtm@users.noreply.github.com>
2023-01-10 23:05:10 +0700

    Remove unsed file

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/taihtm/Documents/buidler/buidler-documentation/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/taihtm/Documents/buidler/buidler-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/taihtm/Documents/buidler/buidler-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/taihtm/Documents/buidler/buidler-documentation/src/css/custom.css'),
-];
