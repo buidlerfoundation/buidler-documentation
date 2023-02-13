f13951d19a2737023ef0fab925a4b58d08432bbb
Khanh Hung <>
2023-02-13 12:16:01 +0700

    change image size and update content create wallet article

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,13 +3,48 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '4a3'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '21e'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '557'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'f63'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', 'c48'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '0a8'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '693'),
+    exact: true
+  },
+  {
     path: '/markdown-page',
     component: ComponentCreator('/markdown-page', '491'),
     exact: true
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '20d'),
+    component: ComponentCreator('/docs', '2fd'),
     routes: [
       {
         path: '/docs/blog/discord-in-web3',
@@ -67,7 +102,7 @@ export default [
       },
       {
         path: '/docs/using-buidler/how-to-create-wallet',
-        component: ComponentCreator('/docs/using-buidler/how-to-create-wallet', '7bb'),
+        component: ComponentCreator('/docs/using-buidler/how-to-create-wallet', '255'),
         exact: true,
         sidebar: "tutorialSidebar"
       }
