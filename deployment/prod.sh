21766b8b7011421b1e684d565151b11a2e6c3723
Ryan <31500999+taihtm@users.noreply.github.com>
2023-02-18 03:31:28 +0700

    Update cicd

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-_dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-exec TAGS="$1" docker compose \
-  -f "$_dockerDir/compose.prod.yml" \
+# _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# exec TAGS="$1" docker compose \
+#   -f "$_dockerDir/compose.prod.yml" \
 
-# sudo docker compose -f compose.prod.yml up -d
-# sudo pwd
+exec echo "$1"
