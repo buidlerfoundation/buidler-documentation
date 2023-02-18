a667446acd9d956fddc479d04bc37c4492ca8215
Ryan <31500999+taihtm@users.noreply.github.com>
2023-02-18 23:27:26 +0700

    Update cicd

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+_tags=$1
+
+echo "Deployment docker image tag is: $_tags"
+
+docker login --username=taihtm --password=Minhtai1908
 
 export TAGS=$_tags
 exec docker compose -p buidler-documentation-prod \
