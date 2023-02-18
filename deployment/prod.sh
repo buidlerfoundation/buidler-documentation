58b0c62cbf2a4e3aa31bc466da840de18188c39c
Ryan <31500999+taihtm@users.noreply.github.com>
2023-02-18 12:07:18 +0700

    Update cicd

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -4,6 +4,6 @@ _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 _tags=$1
 
 
-exec TAGS=_tags docker compose \
+exec TAGS=$_tags docker compose \
   -f "$_dockerDir/compose.prod.yml" \
   "$@"
