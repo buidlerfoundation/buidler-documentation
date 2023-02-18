6ed1331cd26e167f1eadd72376d428bb084dadc4
Ryan <31500999+taihtm@users.noreply.github.com>
2023-02-18 22:57:01 +0700

    Update cicd

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
+
 _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo $1
-echo $_dockerDir
-
-# export TAGS=$_tags
-# exec docker compose -p buidler-documentation-prod \
-#   -f "$_dockerDir/compose.prod.yml" \
-#   "${@: 2}"
+export TAGS=$_tags
+exec docker compose -p buidler-documentation-prod \
+  -f "$_dockerDir/compose.prod.yml" \
+  "${@: 2}"
