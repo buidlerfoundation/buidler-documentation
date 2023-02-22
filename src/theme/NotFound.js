ee2227bcb6494e77aebf8be7b5a605af2f1e05ba
HungNguyen <baohung2412@gmail.com>
2023-02-22 07:59:40 +0700

    Update not found page

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import NotFound from '@theme-original/NotFound';
+import Layout from '@theme/Layout';
 
 export default function NotFoundWrapper(props) {
   return (
-    <>
-      <NotFound {...props} />
-    </>
-  );
+    <Layout>
+      <div className="container margin-vert--xl container-404">
+        <span className="page-content content-desktop">Oops! Page Not Found</span>
+        <span className="page-content content-mobile">Oops!<br />Page Not Found</span>
+        <span className="page-description ml-auto mr-auto">
+          It seems like we can't find the page you are looking for. Let's head
+          back home or try other links instead.
+        </span>
+        <a
+          className="join-community btn-desktop"
+          style={{marginTop: 45}}
+          href="/"
+          >Back To Home</a>
+        <a
+          className="join-community btn-mobile"
+          style={{marginTop: 80}}
+          href="/"
+          >Back To Home</a>
+      </div>
+    </Layout>
+  )
 }
