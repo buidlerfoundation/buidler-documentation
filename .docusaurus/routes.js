21ba076e606d6a652910b702c8d40a1decf8c535
edricnguyen.eth <khanhhungnguyen211@gmail.com>
2023-05-31 21:10:41 +0700

    added multi-chain intro

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '4a3'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '21e'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '557'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'f63'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', 'c48'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '0a8'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '693'),
+    exact: true
+  },
+  {
     path: '/404',
     component: ComponentCreator('/404', 'fad'),
     exact: true
@@ -14,7 +49,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '82d'),
+    component: ComponentCreator('/docs', 'f2c'),
     routes: [
       {
         path: '/docs/community/how-to-join-community',
@@ -31,6 +66,12 @@ export default [
       {
         path: '/docs/general/exclusive-space',
         component: ComponentCreator('/docs/general/exclusive-space', 'a58'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/general/multi-chain-citizens',
+        component: ComponentCreator('/docs/general/multi-chain-citizens', '59b'),
         exact: true,
         sidebar: "tutorialSidebar"
       },
