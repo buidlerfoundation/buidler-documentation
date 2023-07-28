a388454b574743bd5c96904dba58653ec4fcfcc4
edricnguyen.eth <khanhhungnguyen211@gmail.com>
2023-07-28 14:18:22 +0700

    Update search algolia config

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -65,7 +65,7 @@ export default {
       "contextualSearch": false,
       "externalUrlRegex": "external\\.com|domain\\.com",
       "replaceSearchResultPathname": {
-        "from": "/docs/",
+        "from": "/",
         "to": "/"
       },
       "searchParameters": {
